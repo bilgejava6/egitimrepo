1db9fece0aefe2686b7c9c7820be28233db734de
ErenDuzel <duzel.eren96@gmail.com>
2022-12-21 10:49:12 +0300

    For dongusu eklendi.

--- ErenDuzel/src/main/java/com/erenduzel/Main.java
+++ ErenDuzel/src/main/java/com/erenduzel/Main.java
@@ -2,6 +2,9 @@ package com.erenduzel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Merhaba Ben Eren Duzel");
+        for(int i=0;i<10;i++){
+            System.out.println("i....:"+i);
+        }
+        }
 }
