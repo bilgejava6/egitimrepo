36b136a93bf2e20c5d5e6d9e1eb03b735b9970f0
abdullahsokmen <Abdu11ahsk@icloud.com>
2022-12-21 10:21:33 +0300

    Ana sayfayı değiştirdim

--- AbdullahSokmen/src/main/java/com/abdullahsokmen/Main.java
+++ AbdullahSokmen/src/main/java/com/abdullahsokmen/Main.java
@@ -1,7 +1,13 @@
 package com.abdullahsokmen;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)    {
+
+
+
+        System.out.println("Merhaba ben Abdullah!");
     }
+
+
+
 }
