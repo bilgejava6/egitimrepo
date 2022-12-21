8a4f9cf14193e018a2b5c5fe259d99c501a14c71
akkulahali <aliakkulahh@gmail.com>
2022-12-21 10:48:45 +0300

    For döngüsü eklendi.

--- AliAkkulah/src/main/java/com/aliakkulah/Main.java
+++ AliAkkulah/src/main/java/com/aliakkulah/Main.java
@@ -2,6 +2,9 @@ package com.aliakkulah;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Merhaba Ben Ali Akkülah");
+        for(int i = 0; i<10; i++){
+            System.out.println("i....:" + i);
+        }
     }
 }
