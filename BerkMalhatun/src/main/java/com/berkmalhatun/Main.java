40d9196b9d4d869db339740203b74ed76cca15b3
Berkmalhatun <brkmalhatun@gmail.com>
2022-12-21 10:49:39 +0300

    For döngüsü eklendi.

--- BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
+++ BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
@@ -2,6 +2,10 @@ package com.berkmalhatun;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Merhaba Ben Berk");
+    for (int i = 0;i<10;i++){
+        System.out.println("i....:"+i);
+             }
+         }
+
     }
-}
