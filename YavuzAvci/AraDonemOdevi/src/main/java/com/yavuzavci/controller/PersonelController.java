7f7560031efba62454dcdae194b7ef48b4823b5b
yavuzavci <yavuzavci@gmail.com>
2022-12-28 19:25:55 +0300

    controller ve service katmanları düzenlendi

--- YavuzAvci/AraDonemOdevi/src/main/java/com/yavuzavci/controller/PersonelController.java
+++ YavuzAvci/AraDonemOdevi/src/main/java/com/yavuzavci/controller/PersonelController.java
@@ -1,13 +1,13 @@
 package com.yavuzavci.controller;
 
 import static com.yavuzavci.entity.ECinsiyet.*;
+import static com.yavuzavci.utility.PersonelUtility.*;
 
-import com.yavuzavci.entity.BuroPersoneli;
-import com.yavuzavci.entity.Departman;
-import com.yavuzavci.entity.Personel;
+import com.yavuzavci.entity.*;
 import com.yavuzavci.service.PersonelService;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PersonelController {
     private final PersonelService personelService;
@@ -17,15 +17,51 @@ public class PersonelController {
     }
 
     public void save(){
-        Personel personel = new BuroPersoneli("", DIGER,0,"",new Departman(""));
-        personelService.save(personel);
+        // her personel için geçerli
+        String adSoyad = adSoyadAl();
+        ECinsiyet cinsiyet = cinsiyetAl();
+        Integer yas = yasAl();
+        String telefon = telefonAl();
+        Departman departman = departmanAl();
+        // personel türüne göre değişen işlemler
+        Personel personel = null;
+        switch (yeniPersonelTuruSec()){
+            case 1:
+                personel = new BuroPersoneli(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 2:
+                personel = new GenelMudur(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 3:
+                personel = new Hizmetli(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 4:
+                personel = new InsanKaynaklariPersoneli(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 5:
+                personel = new Mudur(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 6:
+                personel = new MuhasebePersoneli(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            case 7:
+                personel = new TeknikPersonel(adSoyad,cinsiyet,yas,telefon,departman);
+                break;
+            default:
+                System.out.println("HATA: Yanlış personel türü seçtiniz.");
+                break;
+        }
+        if(Objects.isNull(personel))
+            System.out.println("İşlem başarısız.");
+        else
+            personelService.save(personel);
     }
     public void update(){
         Personel personel = new BuroPersoneli("", DIGER,0,"",new Departman(""));
         personelService.update(personel);
     }
     public void delete() {
-        Long id = 0L;
+        Long id = silinecekPersonelIdAl();
         personelService.delete(id);
     }
     public Personel findById(Long id){
