8ac595888b711718d48ddf6cf9a0d268180c776b
yavuzavci <yavuzavci@gmail.com>
2023-01-04 09:54:38 +0300

    son degisiklik

--- YavuzAvci/src/main/java/com/yavuzavci/Main.java
+++ YavuzAvci/src/main/java/com/yavuzavci/Main.java
@@ -6,5 +6,6 @@ public class Main {
         for (int i = 0; i < 10 ; i++){
             System.out.println("i....: " + i);
         }
+        System.out.println("Program sonlandırıldı.");
     }
 }
