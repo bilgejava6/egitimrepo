f786291296f7a645d02ce07f068785019b4167b0
ErincK <erinckocaoglu@gmail.com>
2023-01-04 09:54:20 +0300

    04.01.23

---
+++ ErincKocaoglu/src/main/java/com/Exceptions_Istisnalar/Runner.java
@@ -0,0 +1,19 @@
+package com.Exceptions_Istisnalar;
+
+import java.util.Scanner;
+
+public class Runner {
+
+        System.out.println("************************");
+        System.out.println("***  Hesap Makinesi ****");
+        System.out.println("************************");
+        System.out.println("1- Toplama");
+        System.out.println("2- Cikartma");
+        System.out.println("3- Carpma");
+        System.out.println("4- Bolme");
+        System.out.println("0- Cikis");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Secim yap lutfen.");
+        int secim = sc.nextInt();
+
+}
