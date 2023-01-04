364087b64fcc203f971a9a255b20abf6542d5189
abdullahsokmen <Abdu11ahsk@icloud.com>
2023-01-04 09:53:57 +0300

    Update

--- AbdullahSokmen/src/main/java/com/abdullahsokmen/Main.java
+++ AbdullahSokmen/src/main/java/com/abdullahsokmen/Main.java
@@ -2,16 +2,12 @@ package com.abdullahsokmen;
 
 public class Main {
     public static void main(String[] args)    {
-
-
-
         System.out.println("Merhaba ben Abdullah!");
         for(int i=0;i<10;i++){
             System.out.println("i........:"+i);
         }
         System.out.println("update");
-
-
+        System.out.println("Bitti");
     }
 
 
