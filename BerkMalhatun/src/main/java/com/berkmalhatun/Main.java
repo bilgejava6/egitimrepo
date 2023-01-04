ee09e7ebd1dba052db01f8f2b2be708667d5e125
Berk <brkmalhatun@gmail.com>
2023-01-04 09:58:57 +0300

    son islemler

--- BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
+++ BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bitti.");
         System.out.println("bittti");
+        System.out.println("bitiyor");
     }
 
 }
