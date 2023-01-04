77a2131935d2c179ceaa285ad87ffc5a60fe018d
ErincK <erinckocaoglu@gmail.com>
2023-01-04 10:25:51 +0300

    04.01.23

--- ErincKocaoglu/src/main/java/com/Exceptions_Istisnalar/Runner.java
+++ ErincKocaoglu/src/main/java/com/Exceptions_Istisnalar/Runner.java
@@ -3,17 +3,19 @@ package com.Exceptions_Istisnalar;
 import java.util.Scanner;
 
 public class Runner {
+        public static void main(String[] args) {
+                System.out.println("************************");
+                System.out.println("***  Hesap Makinesi ****");
+                System.out.println("************************");
+                System.out.println("1- Toplama");
+                System.out.println("2- Cikartma");
+                System.out.println("3- Carpma");
+                System.out.println("4- Bolme");
+                System.out.println("0- Cikis");
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Secim yap lutfen.");
+                int secim = sc.nextInt();
+        }
 
-        System.out.println("************************");
-        System.out.println("***  Hesap Makinesi ****");
-        System.out.println("************************");
-        System.out.println("1- Toplama");
-        System.out.println("2- Cikartma");
-        System.out.println("3- Carpma");
-        System.out.println("4- Bolme");
-        System.out.println("0- Cikis");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Secim yap lutfen.");
-        int secim = sc.nextInt();
 
 }
