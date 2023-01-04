064553da1e45fcf25d482922a97ad4ecbcbfb467
Berk <brkmalhatun@gmail.com>
2023-01-04 09:49:23 +0300

    son islemler

--- BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
+++ BerkMalhatun/src/main/java/com/berkmalhatun/Main.java
@@ -1,11 +1,11 @@
 package com.berkmalhatun;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Merhaba Ben Berk");
-    for (int i = 0;i<10;i++){
-        System.out.println("i....:"+i);
-             }
-         }
-
+        System.out.println("bitti.");
     }
+
+}
